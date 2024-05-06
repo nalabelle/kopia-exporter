0374861e4cea8339120fd6c16f24cd2bc1e2601c
nalabelle <git@nalabelle.com>
2024-05-06 21:44:57 +0000

    refactor: break up main, add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
 use std::io;
-use std::collections::HashMap;
 use std::io::Read;
-use std::mem;
-use serde::Deserialize;
-use chrono::DateTime;
-use chrono::offset::Utc;
 
+mod stats;
+mod kopia;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-struct BackupStats {
-    end_time: DateTime<Utc>,
-    error_count: u64,
-    total_size: u64,
-}
-
-type Backups = HashMap<Source, BackupStats>;
-
-#[derive(Deserialize)]
-struct KopiaStats {
-    #[serde(rename = "totalSize")]
-    total_size: u64,
-    #[serde(rename = "errorCount")]
-    error_count: u64,
-}
-
-#[derive(Deserialize,Hash, PartialEq, Eq, PartialOrd, Ord, Default)]
-struct Source {
-    host: String,
-    #[serde(rename = "userName")]
-    user: String,
-    path: String,
-}
-
-impl Source {
-    fn take(&mut self) -> Self {
-        mem::take(self)
-    }
-}
-
-#[derive(Deserialize)]
-struct KopiaBackup {
-    source: Source,
-    #[serde(rename = "endTime")]
-    end_time: DateTime<Utc>,
-    stats: KopiaStats,
-}
+use crate::kopia::BackupsFromJson;
+use crate::stats::BackupsCollect;
 
 fn read_input() -> String {
     let mut buf = String::new();
@@ -53,23 +14,14 @@ fn read_input() -> String {
     buf
 }
 
-fn collect(backups: &Backups) {
-    for (source, details) in backups.iter() {
-        let tags = format!("host=\"{}\",user=\"{}\",path=\"{}\"", source.host, source.user, source.path);
-        println!("kopia_backup_run{{{}}} {}", tags, details.end_time);
-        println!("kopia_backup_size{{{}}} {}", tags, details.total_size);
-        println!("kopia_backup_errors{{{}}} {}", tags, details.error_count);
-    }
-}
-
 fn main() {
     let input = read_input();
-    let mut kopia_backups: Vec<KopiaBackup> = serde_json::from_str(&input).unwrap();
-    let mut latest: Backups = HashMap::new();
+    let mut kopia_backups: kopia::Backups = kopia::Backups::from_json(&input);
+    let mut latest: stats::Backups = stats::Backups::new();
 
     for kopia_backup in kopia_backups.iter_mut() {
         let source = kopia_backup.source.take();
-        let details = BackupStats {
+        let details = stats::Stats {
             end_time: kopia_backup.end_time,
             error_count: kopia_backup.stats.error_count,
             total_size: kopia_backup.stats.total_size,
@@ -85,5 +37,5 @@ fn main() {
             }
         }
     }
-    collect(&latest);
+    latest.collect(&mut io::stdout());
 }
