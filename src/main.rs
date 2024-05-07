92ad2de55876878bf6fb353a869dbeaf0118de9b
nalabelle <git@nalabelle.com>
2024-05-07 00:45:36 +0000

    feat: move more logic out of main

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,7 @@ fn read_input() -> String {
 }
 
 fn main() {
-    let input = read_input();
-    let mut kopia_backups: kopia::Backups = kopia::Backups::from_json(&input);
-    let mut latest: stats::Backups = stats::Backups::new();
-
-    for kopia_backup in kopia_backups.iter_mut() {
-        let source = kopia_backup.source.take();
-        let details = stats::Stats {
-            end_time: kopia_backup.end_time,
-            error_count: kopia_backup.stats.error_count,
-            total_size: kopia_backup.stats.total_size,
-        };
-        match latest.get(&source) {
-            Some(stored_details) => {
-                if &details > stored_details {
-                    latest.insert(source, details);
-                }
-            },
-            None => {
-                latest.insert(source, details);
-            }
-        }
-    }
+    let mut kopia_backups: kopia::Backups = kopia::Backups::from_json(&read_input());
+    let latest: stats::Backups = stats::Backups::from_kopia(&mut kopia_backups);
     latest.collect(&mut io::stdout());
 }
